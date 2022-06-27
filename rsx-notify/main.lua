6af2c6089d7f457ed7f3971889636503592bd5ec
Jaareet <80985233+Jaareet@users.noreply.github.com>
2022-06-27 22:30:45 +0100

    Añadido cola notis lua

--- rsx-notify/main.lua
+++ rsx-notify/main.lua
@@ -1,16 +1,17 @@
-exports('sendnotification', function(type, text)
-    sendnoti(type, text)
-end)
+local Notifications_Boolean = false
+
+exports('sendnotification', sendnoti)
 
 function sendnoti(type, text)
+    while (Notifications_Boolean) do
+        Citizen.Wait(100)
+    end
     SendNUIMessage({
         type = type,
         msg = text,
     })
-    print("sent")   
 end
 
 RegisterCommand('testnoti', function(source, args)
     sendnoti(args[1], args[2])
 end)
-
